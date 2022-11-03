f0683cffa6b66fe65733e2ab2b1caf06edae2bf2
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-03 23:59:38 +0200

    lisätty delete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const URL = 'http://localhost/todo/';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
+  const [newTask, setNewTask] = useState('')
   useEffect(() => {
     axios.get(URL)
     .then((response) => {
@@ -16,15 +16,53 @@ function App() {
     })
   }, [])
 
+  const save = (e) => {
+    e.preventDefault()
+    const json = JSON.stringify({description:newTask})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    .then((response) => {
+      setTasks([...tasks,response.data])
+      setNewTask('')
+    }).catch(error => {
+       console.log(error.response ? error.response.data.error : error)
+       alert('häiriö') 
+    })
+  }
 
-
-
+function remove(id) {
+  const json = JSON.stringify({id:id})
+  axios.post(URL + 'delete.php', json,{
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+  .then((response) => {
+    const newListWithoutRemoved = tasks.filter((item) => item.id !==id);
+    setTasks(newListWithoutRemoved);
+  }).catch (error => {
+    alert(error.response ? error.response.data.error : error);
+  })
+}
 
   return (
     <div className='container'>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={newTask} onChange={e => setNewTask(e.target.value)} />
+        <button>Save</button>
+      </form>
       <ol>
         {tasks?.map(task => (
-          <li key={task.id}>{task.description}</li>
+          <li key={task.id}>
+            {task.description}&nbsp;
+            <a href="#" className='delete' onClick={() => remove(task.id)}>
+              Delete
+            </a>
+            </li>
         ))}
       </ol>
     </div>
