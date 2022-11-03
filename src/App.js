22d5532162b326fcf51a183b848a70c96c5443b3
markov76 <101133846+markov76@users.noreply.github.com>
2022-11-03 22:05:12 +0200

    korjauksia

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import {useEffect, useState} from 'react'
+
+const URL = 'http://localhost/todo/';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setTasks(response.data)
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }, [])
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <ol>
+        {tasks?.map(task => (
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
     </div>
-  );
+  ); 
 }
 
 export default App;
